9aaad16863f4549dcf4cb1ad431e42d030d20c48
pdrana82732-dot <pdrana82732@gmail.com>
2026-05-25 15:30:53 +0530

    Add files via upload

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,15 @@
-function NotFound() {
+export default function NotFound() {
     return (
-        <div style={{ padding: "50px", textAlign: "center" }}>
+        <div
+            style={{
+                paddingTop: "140px",
+                paddingBottom: "100px",
+                textAlign: "center",
+                minHeight: "100vh",
+            }}
+        >
             <h1>404 - Page Not Found</h1>
             <p>The page you are looking for does not exist.</p>
         </div>
-    );
+    )
 }
-
-export default NotFound;
